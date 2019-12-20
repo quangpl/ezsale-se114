9c9eeb06d23e7cb57386b7bb374161af8117d586
Truong Dieu Linh <17520691>
2019-12-20 16:22:55 +0700

    edit PersonalScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,34 +1,95 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import { View, StyleSheet ,Text,Button} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import {Input,Image} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-import HorizontalItem from "../components/HorizontalItem";
+
 export default function LoginScreen() {
   return (
-    <ScrollView style={styles.container}>
-     <Text>Login</Text>
-    </ScrollView>
+  <View style={styles.container}>
+    <View style ={styles.img}>
+      <Image
+          source={{ uri: "https://image.flaticon.com/icons/png/512/584/584093.png" }}
+          style={{ width: 150, height: 150 , color: "#fff" }}/>
+    </View>
+
+
+    <View style ={styles.title}>
+      <Text style={styles.text}>Đăng nhập</Text>
+    </View>
+
+    <View style ={styles.userInfo}>
+      <Input placeholder='  Tên đăng nhập / email' leftIcon={<Icon name='user' size={24} color='#fff'/>}/>
+    </View>
+
+    <View style ={styles.userInfo}>
+      <Input placeholder='  Password' leftIcon={<Icon name='lock' size={24} color='#fff'/>}/>
+
+    </View>
+
+
+    <View style ={styles.btn}>
+      <Button size="large" title="Login" />
+    </View>
+  </View>
   );
 }
 
-LoginScreen.navigationOptions = {
-  title: "Login",
-  headerTintColor: "white",
-  headerBackground: (
-    <LinearGradient
-      colors={["#237AE4", "#6C5CE7"]}
-      style={{ flex: 1, borderWidth: 0 }}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 0 }}
-    />
-  )
-};
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#F8F9FA"
+    flex:1,
+    backgroundColor:"#fff",
+    flexDirection: "column"
+
+  },
+  img:{
+    flex: 0.3,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+
+  text:{
+    color:"black",
+    fontSize: 22,
+    fontWeight: "bold"
+  },
+  title:{
+    color: "#fff",
+    flex: 0.1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+  },
+
+  userInfo: {
+    flex: 0.1,
+    flexDirection: "row",
+  },
+
+  btn:{
+    flex: 0.13,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
   }
+
+
 });
+
+
+// LoginScreen.navigationOptions = {
+//   title: "Login",
+//   headerTintColor: "white",
+//   headerBackground: (
+//     <LinearGradient
+//       colors={["#237AE4", "#6C5CE7"]}
+//       style={{ flex: 1, borderWidth: 0 }}
+//       start={{ x: 0, y: 0 }}
+//       end={{ x: 1, y: 0 }}/>
+//   )
+// };
