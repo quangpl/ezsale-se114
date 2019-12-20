0f1eda7957115696201ff481414bc535070f5c2a
Phan Quang <phanlequang99@gmail.com>
2019-12-20 22:20:41 +0700

    Add loogin

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,95 +1,119 @@
 import React from "react";
-import { View, StyleSheet ,Text,Button} from "react-native";
+import {TextInput, View, StyleSheet ,Text} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import {Input,Image} from 'react-native-elements';
+import { Image, Button } from "react-native-elements";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
-export default function LoginScreen() {
-  return (
-  <View style={styles.container}>
-    <View style ={styles.img}>
-      <Image
-          source={{ uri: "https://image.flaticon.com/icons/png/512/584/584093.png" }}
-          style={{ width: 150, height: 150 , color: "#fff" }}/>
-    </View>
+export default class LoginScreen extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Image
+            source={require("../assets/images/cart.png")}
+            style={{ width: 70, height: 70 }}
+          />
+          <Text style={styles.slogan}>Catch sale and sale your money !</Text>
+        </View>
 
+        <View style={styles.title}>
+          <Text style={styles.text}>Đăng nhập</Text>
+        </View>
 
-    <View style ={styles.title}>
-      <Text style={styles.text}>Đăng nhập</Text>
-    </View>
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Password" />
 
-    <View style ={styles.userInfo}>
-      <Input placeholder='  Tên đăng nhập / email' leftIcon={<Icon name='user' size={24} color='#fff'/>}/>
-    </View>
-
-    <View style ={styles.userInfo}>
-      <Input placeholder='  Password' leftIcon={<Icon name='lock' size={24} color='#fff'/>}/>
-
-    </View>
-
-
-    <View style ={styles.btn}>
-      <Button size="large" title="Login" />
-    </View>
-  </View>
-  );
+        <View style={styles.btnGroup}>
+          <Button
+            buttonStyle={styles.btn}
+            size="large"
+            type="outline"
+            title="Đăng nhập"
+          />
+          <Button
+            buttonStyle={styles.btnReg}
+            size="large"
+            type="clear"
+            sty
+            titleStyle={{
+              color:"#fff"
+            }}
+            title="Đăng ký"
+          />
+        </View>
+      </View>
+    );
+  }
+  
 }
+ 
 
+LoginScreen.navigationOptions = {
+  header: null
+};
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor:"#fff",
+    flex: 1,
+    backgroundColor: "#fff",
     flexDirection: "column"
+  },
+  logo: {
+    flex: 0.5,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignContent: "flex-end",
+    alignItems: "center"
+  },
 
+  text: {
+    color: "white",
+    fontSize: 22
   },
-  img:{
-    flex: 0.3,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
+  slogan: {
+    color: "#959595",
+    fontSize: 15
   },
 
-  text:{
-    color:"black",
-    fontSize: 22,
-    fontWeight: "bold"
+  input: {
+    marginVertical: 10,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    height: 45,
+    marginHorizontal: 14,
+    borderWidth: 1,
+    borderColor: "#189DFF",
+    paddingLeft: 10
   },
-  title:{
+  title: {
     color: "#fff",
     flex: 0.1,
     flexDirection: "row",
     justifyContent: "center",
     alignContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
 
   userInfo: {
     flex: 0.1,
-    flexDirection: "row",
+    flexDirection: "row"
   },
 
-  btn:{
-    flex: 0.13,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
+  btn: {
+    borderRadius: 25,
+    width: 200,
+    alignSelf: "center"
+  },
+  btnReg:{
+    borderRadius: 25,
+    width: 200,
+    alignSelf: "center",
+    marginTop:20,
+    backgroundColor:"#189DFF",
+    color:"#fff"
+  },
+  btnGroup:{
+    marginTop:20,
   }
-
-
 });
 
-
-// LoginScreen.navigationOptions = {
-//   title: "Login",
-//   headerTintColor: "white",
-//   headerBackground: (
-//     <LinearGradient
-//       colors={["#237AE4", "#6C5CE7"]}
-//       style={{ flex: 1, borderWidth: 0 }}
-//       start={{ x: 0, y: 0 }}
-//       end={{ x: 1, y: 0 }}/>
-//   )
-// };
